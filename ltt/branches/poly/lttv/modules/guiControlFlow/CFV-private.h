72c44f9e4aa5ff8ca49bb071dccfd1c1355bf6f2
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2003-10-13 04:56:03 +0000

    missing file
    git-svn-id: http://ltt.polymtl.ca/svn@300 04897980-b3bd-0310-b5e0-8ef037075253

---
+++ ltt/branches/poly/lttv/modules/guiControlFlow/CFV-private.h
@@ -0,0 +1,37 @@
+#ifndef _CFV_PRIVATE_H
+#define _CFV_PRIVATE_H
+
+
+
+struct _ControlFlowData {
+
+	GtkWidget *Scrolled_Window_VC;
+	
+	ProcessList *Process_List;
+	Drawing_t *Drawing;
+
+	//GtkWidget *HBox_V;
+	GtkWidget *Inside_HBox_V;
+
+	GtkAdjustment *VAdjust_C ;
+	
+	/* Trace information */
+	//TraceSet *Trace_Set;
+	//TraceStatistics *Trace_Statistics;
+	
+	/* Shown events information */
+	guint First_Event, Last_Event;
+	TimeWindow Time_Window;
+	LttTime Current_Time;
+	
+	
+	/* TEST DATA, TO BE READ FROM THE TRACE */
+	gint Number_Of_Events ;
+	guint Currently_Selected_Event  ;
+	gboolean Selected_Event ;
+	guint Number_Of_Process;
+
+} ;
+
+
+#endif //_CFV_PRIVATE_H
