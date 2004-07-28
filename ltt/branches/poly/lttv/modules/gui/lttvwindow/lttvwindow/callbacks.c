6d677a86a688c9b468d934496323e6ea43fc0a01
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2004-07-28 22:10:44 +0000

    fix module unload oops
    git-svn-id: http://ltt.polymtl.ca/svn@644 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt/branches/poly/lttv/modules/gui/lttvwindow/lttvwindow/callbacks.c
+++ ltt/branches/poly/lttv/modules/gui/lttvwindow/lttvwindow/callbacks.c
@@ -2721,9 +2721,9 @@ on_unload_module_activate              (GtkMenuItem     *menuitem,
     GPtrArray *name;
     guint nb,i;
     gchar *module_name;
+    nb = lttv_library_module_number(library);
     LttvModuleInfo *module_info = g_new(LttvModuleInfo,nb);
     name = g_ptr_array_new();
-    nb = lttv_library_module_number(library);
     /* ask for the module name */
 
     for(i=0;i<nb;i++){
