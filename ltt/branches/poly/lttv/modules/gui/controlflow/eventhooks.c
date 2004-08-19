cd3892fef868e57ca950bcee7cceabd7dcf2c61f
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2004-08-19 05:33:41 +0000

    sorting doesn't seem to be so heavy on cpu finally...
    git-svn-id: http://ltt.polymtl.ca/svn@817 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt/branches/poly/lttv/modules/gui/controlflow/eventhooks.c
+++ ltt/branches/poly/lttv/modules/gui/controlflow/eventhooks.c
@@ -3168,13 +3168,13 @@ int before_chunk(void *hook_data, void *call_data)
   EventsRequest *events_request = (EventsRequest*)hook_data;
   LttvTracesetState *tss = (LttvTracesetState*)call_data;
   ControlFlowData *cfd = (ControlFlowData*)events_request->viewer_data;
-  
+#if 0  
   /* Desactivate sort */
   gtk_tree_sortable_set_sort_column_id(
       GTK_TREE_SORTABLE(cfd->process_list->list_store),
       TRACE_COLUMN,
       GTK_SORT_ASCENDING);
- 
+#endif //0
   drawing_chunk_begin(events_request, tss);
 
   return 0;
@@ -3275,7 +3275,7 @@ int after_chunk(void *hook_data, void *call_data)
   /* Draw last items */
   g_hash_table_foreach(process_list->process_hash, draw_closure,
                         (void*)&closure_data);
-
+#if 0
   /* Reactivate sort */
   gtk_tree_sortable_set_sort_column_id(
       GTK_TREE_SORTABLE(control_flow_data->process_list->list_store),
@@ -3283,8 +3283,10 @@ int after_chunk(void *hook_data, void *call_data)
       GTK_SORT_ASCENDING);
 
   update_index_to_pixmap(control_flow_data->process_list);
- 
-  /* Request expose */
+  /* Request a full expose : drawing scrambled */
+  gtk_widget_queue_draw(control_flow_data->drawing->drawing_area);
+#endif //0
+  /* Request expose (updates damages zone also) */
   drawing_request_expose(events_request, tss, end_time);
 
   return 0;
