2cdb6fcba2deb41e6e894cae0f46d9a58a8c9e85
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2005-06-14 18:54:23 +0000

    add lttctl
    git-svn-id: http://ltt.polymtl.ca/svn@952 04897980-b3bd-0310-b5e0-8ef037075253

---
+++ ltt/branches/poly/lttctl/lttctl.c
@@ -0,0 +1,13 @@
+/* lttctl
+ *
+ * Linux Trace Toolkit Control
+ *
+ * Small program that controls LTT through libltt.
+ *
+ * Copyright 2005 -
+ * 	Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
+ */
+
+#include <ltt/libltt.h>
+
+
