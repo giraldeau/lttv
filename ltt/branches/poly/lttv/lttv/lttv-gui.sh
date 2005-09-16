8785384181c79afafdec8acf928ecaf383b31760
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2005-09-16 15:16:09 +0000

    lttv-gui fix
    git-svn-id: http://ltt.polymtl.ca/svn@1209 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt/branches/poly/lttv/lttv/lttv-gui.sh
+++ ltt/branches/poly/lttv/lttv/lttv-gui.sh
@@ -3,6 +3,8 @@
 # This is a simple script that starts lttv with default GUI modules
 # Mathieu Desnoyers 15-09-2005
 
-$0.real -m guievents -m guifilter -m guicontrolflow -m guistatistics \
+LTTV_CMD=`echo $0 | sed 's/-gui$//'`
+
+$LTTV_CMD.real -m guievents -m guifilter -m guicontrolflow -m guistatistics \
     -m guitracecontrol $*
 
