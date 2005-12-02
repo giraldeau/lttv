8c9e90761018e38fbebdf080b9f06afbf17f1ecd
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2005-12-02 22:46:56 +0000

    genevent fix
    git-svn-id: http://ltt.polymtl.ca/svn@1367 04897980-b3bd-0310-b5e0-8ef037075253

--- genevent-new/genevent.c
+++ genevent-new/genevent.c
@@ -453,6 +453,7 @@ int print_type_declaration(type_descriptor_t * td, FILE *fd, unsigned int tabs,
 						fd, tabs, basename, "")) return 1;
 			fprintf(fd, " len;\n");
 			print_tabs(1, fd);
+			fprintf(fd, "const ");
 			if(print_type(((field_t*)td->fields.array[1])->type,
 						fd, tabs, basename, "")) return 1;
 			fprintf(fd, " *array;\n");
