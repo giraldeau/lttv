2bc8cfb7c1c3b85a7d88c42914e1d08385f845d8
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2005-12-02 23:33:22 +0000

    genevent fix
    git-svn-id: http://ltt.polymtl.ca/svn@1368 04897980-b3bd-0310-b5e0-8ef037075253

--- genevent-new/genevent.c
+++ genevent-new/genevent.c
@@ -151,7 +151,7 @@ int print_type(type_descriptor_t * td, FILE *fd, unsigned int tabs,
 			fprintf(fd, "%s", floatOutputTypes[getSizeindex(td->size)]);
 			break;
 		case POINTER:
-			fprintf(fd, "const void *");
+			fprintf(fd, "void *");
 			break;
 		case LONG:
 			fprintf(fd, "long");
@@ -259,7 +259,7 @@ int print_arg(type_descriptor_t * td, FILE *fd, unsigned int tabs,
 			fprintf(fd, " %s", field_name);
 			break;
 		case POINTER:
-			fprintf(fd, "const void *");
+			fprintf(fd, "void *");
 			fprintf(fd, " %s", field_name);
 			break;
 		case LONG:
