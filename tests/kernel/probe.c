0efac34cbfc6de25a57f58700d9c04762a3a7022
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2006-09-21 23:18:48 +0000

    probe test
    git-svn-id: http://ltt.polymtl.ca/svn@2134 04897980-b3bd-0310-b5e0-8ef037075253

--- tests/kernel/probe.c
+++ tests/kernel/probe.c
@@ -13,14 +13,17 @@
 #include <linux/kallsyms.h>
 
 /* function to install */
+#define DO_MARK1_FORMAT "%d"
 asmlinkage void do_mark1(const char *format, int value)
 {
+	__mark_check_format(DO_MARK1_FORMAT, value);
 	printk("value is %d\n", value);
 }
 
 int init_module(void)
 {
-	return marker_set_probe("subsys_mark1", (marker_probe_func*)do_mark1,
+	return marker_set_probe("subsys_mark1", DO_MARK1_FORMAT,
+			(marker_probe_func*)do_mark1,
 			MARKER_CALL);
 }
 
