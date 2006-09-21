3bb4bef88faa8857b10f687047932e0548f2d310
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2006-09-21 15:41:43 +0000

    probe test
    git-svn-id: http://ltt.polymtl.ca/svn@2131 04897980-b3bd-0310-b5e0-8ef037075253

---
+++ tests/kernel/probe.c
@@ -0,0 +1,36 @@
+/* probe.c
+ *
+ * Loads a function at a marker call site.
+ *
+ * (C) Copyright 2006 Mathieu Desnoyers <mathieu.desnoyers@polymtl.ca>
+ *
+ * This file is released under the GPLv2.
+ * See the file COPYING for more details.
+ */
+
+#include <linux/marker.h>
+#include <linux/module.h>
+#include <linux/kallsyms.h>
+
+/* function to install */
+void do_mark1(const char *format, int value)
+{
+	printk("value is %d\n", value);
+}
+
+int init_module(void)
+{
+	return marker_set_probe("subsys_mark1", (marker_probe)do_mark1,
+			MARKER_CALL);
+}
+
+void cleanup_module(void)
+{
+	marker_disable_probe("subsys_mark1", (marker_probe)do_mark1,
+		MARKER_CALL);
+}
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Mathieu Desnoyers");
+MODULE_DESCRIPTION("Probe");
+
