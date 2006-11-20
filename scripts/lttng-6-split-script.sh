48edb02d4bc8db4b56c888fe697e82d72683c240
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2006-11-20 21:39:38 +0000

    update script
    git-svn-id: http://ltt.polymtl.ca/svn@2261 04897980-b3bd-0310-b5e0-8ef037075253

--- scripts/lttng-6-split-script.sh
+++ scripts/lttng-6-split-script.sh
@@ -48,7 +48,8 @@ VALUE=$(( ${VALUE} + 1 ))
 printf -v COUNT "%02d" ${VALUE}
 
 IN="?_include_linux_relay.h
-?_kernel_relay.c"
+?_kernel_relay.c
+?_block_blktrace.c"
 
 for a in $IN; do wr $a $FILE; done
 
