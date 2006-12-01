de09a23f17301ba0bede3c217a13fb686afed4cc
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2006-12-01 21:06:54 +0000

    update script
    git-svn-id: http://ltt.polymtl.ca/svn@2278 04897980-b3bd-0310-b5e0-8ef037075253

--- scripts/lttng-6-split-script.sh
+++ scripts/lttng-6-split-script.sh
@@ -92,15 +92,21 @@ FILE=../${PRENAME}${COUNT}${NAME}-atomic.diff
 VALUE=$(( ${VALUE} + 1 ))
 printf -v COUNT "%02d" ${VALUE}
 
-IN="?_include_asm-i386_atomic.h
+IN="
+?_include_asm-alpha_atomic.h
+?_include_asm-alpha_system.h
+?_include_asm-arm_atomic.h
+?_include_asm-generic_atomic.h
+?_include_asm-i386_atomic.h
 ?_include_asm-i386_system.h
-?_include_asm-x86_64_atomic.h
-?_include_asm-x86_64_system.h
+?_include_asm-ia64_atomic.h
+?_include_asm-mips_atomic.h
+?_include_asm-parisc_atomic.h
 ?_include_asm-powerpc_atomic.h
 ?_include_asm-powerpc_system.h
-?_include_asm-arm_atomic.h
-?_include_asm-mips_atomic.h
-?_include_asm-generic_atomic.h"
+?_include_asm-sparc64_atomic.h
+?_include_asm-x86_64_atomic.h
+?_include_asm-x86_64_system.h"
 
 for a in $IN; do wr $a $FILE; done
 
@@ -108,11 +114,17 @@ FILE=../${PRENAME}${COUNT}${NAME}-local.diff
 VALUE=$(( ${VALUE} + 1 ))
 printf -v COUNT "%02d" ${VALUE}
 
-IN="?_include_asm-i386_atomic.h
-?_include_asm-x86_64_local.h
-?_include_asm-powerpc_local.h
+IN="
+?_include_asm-alpha_local.h
+?_include_asm-generic_local.h
+?_include_asm-i386_local.h
+?_include_asm-ia64_local.h
 ?_include_asm-mips_local.h
-?_include_asm-generic_local.h"
+?_include_asm-parisc_local.h
+?_include_asm-powerpc_local.h
+?_include_asm-s390_local.h
+?_include_asm-sparc64_local.h
+?_include_asm-x86_64_local.h"
 
 for a in $IN; do wr $a $FILE; done
 
