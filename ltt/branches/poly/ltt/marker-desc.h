29ec67861cc68f03b93a5a4c5a7e2a002cf3170e
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2007-10-11 16:22:27 +0000

    fix marker-desc
    git-svn-id: http://ltt.polymtl.ca/svn@2654 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt/branches/poly/ltt/marker-desc.h
+++ ltt/branches/poly/ltt/marker-desc.h
@@ -8,6 +8,7 @@
  * License: LGPL.
  */
 
+#include <stdio.h>
 #include <glib.h>
 #include <ltt/marker-field.h>
 
@@ -16,7 +17,7 @@ static inline GQuark ltt_enum_string_get(struct marker_field *f,
   gulong value)
 {
   char tmp[1024] = "ENUM-";
-  sprintf(tmp[sizeof("ENUM-") - 1], "%lu", value);
+  sprintf(&tmp[sizeof("ENUM-") - 1], "%lu", value);
   return g_quark_from_string(tmp);
 }
 
