b406b83259b3ae941993726336ce83632983bac3
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2007-11-06 18:42:30 +0000

    update arm/disarm
    git-svn-id: http://ltt.polymtl.ca/svn@2728 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt-control/lttctl/ltt-armall.sh
+++ ltt-control/lttctl/ltt-armall.sh
@@ -3,9 +3,3 @@
 echo Connecting all markers
 MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -v ^core_|grep -v ^locking_`
 for a in $MARKERS; do echo Connecting $a; echo "connect $a default" > /proc/ltt > /proc/ltt; done
-
-#excluding locking markers (high traffic)
-#core markers must be armed
-echo Arming all markers
-MARKERS=`cat /proc/ltt|awk '{print $2}'|sort -u|grep -v ^locking_`
-for a in $MARKERS; do echo Arming $a; echo "arm $a" > /proc/ltt; done
--- ltt-control/lttctl/ltt-disarmall.sh
+++ ltt-control/lttctl/ltt-disarmall.sh
@@ -1,9 +1,4 @@
 #excluding locking
-echo Disarming all markers
-MARKERS=`cat /proc/ltt|awk '{print $2}'|sort -u|grep -v ^locking_`
-for a in $MARKERS; do echo Disarming $a; echo "disarm $a" > /proc/ltt; done
-
-#excluding locking
 #excluding core markers, not connected to default.
 echo Disconnecting all markers
 MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -v ^core_|grep -v ^locking_`
