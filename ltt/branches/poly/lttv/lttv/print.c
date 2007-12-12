c8948b8d1519e45adafe850bb4aa0004fa4f310b
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2007-12-12 19:48:49 +0000

    update print softirq
    git-svn-id: http://ltt.polymtl.ca/svn@2792 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt/branches/poly/lttv/lttv/print.c
+++ ltt/branches/poly/lttv/lttv/print.c
@@ -56,7 +56,8 @@ static inline void print_enum_events(LttEvent *e, struct marker_field *f,
     g_string_append_printf(s, " [%s]",
       g_quark_to_string(ts->syscall_names[value]));
   } else if ((info->name == g_quark_from_static_string("kernel_softirq_entry")
-    || info->name == g_quark_from_static_string("kernel_softirq_exit")) &&
+    || info->name == g_quark_from_static_string("kernel_softirq_exit")
+    || info->name == g_quark_from_static_string("kernel_softirq_raise")) &&
       f->name == g_quark_from_static_string("softirq_id")) {
     g_string_append_printf(s, " [%s]",
       g_quark_to_string(ts->soft_irq_names[value]));
