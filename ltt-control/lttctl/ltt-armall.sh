b5efb4c75625be903d068d9560b78433adc801c7
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2008-01-21 18:39:59 +0000

    update armall
    git-svn-id: http://ltt.polymtl.ca/svn@2804 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt-control/lttctl/ltt-armall.sh
+++ ltt-control/lttctl/ltt-armall.sh
@@ -3,11 +3,10 @@
 
 #scheduler probe
 echo Loading probes
-modprobe ltt-sched
+modprobe -q ltt-sched
 
 echo Connecting all markers
-MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -v ^core_|grep
--v ^locking_ |grep -v ^internal_`
+MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -v ^core_|grep -v ^locking_ |grep -v ^internal_`
 
 echo Connecting internal_kernel_sched_schedule
 echo "connect internal_kernel_sched_schedule scheduler" > /proc/ltt
