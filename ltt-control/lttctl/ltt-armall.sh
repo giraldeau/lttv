98a7929c950661c17fe240cf575b47d76d259cd6
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2008-02-27 13:41:02 +0000

    send markers to correct channels
    git-svn-id: http://ltt.polymtl.ca/svn@2822 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt-control/lttctl/ltt-armall.sh
+++ ltt-control/lttctl/ltt-armall.sh
@@ -11,4 +11,27 @@ MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -v ^core_|grep -
 echo Connecting internal_kernel_sched_schedule
 echo "connect internal_kernel_sched_schedule scheduler" > /proc/ltt
 
-for a in $MARKERS; do echo Connecting $a; echo "connect $a default" > /proc/ltt; done
+for a in $MARKERS; do
+	echo Connecting $a
+
+	#redirect markers carrying state information to dedicated channels
+	case $a in
+	list_process_state|user_generic_thread_brand|fs_exec|kernel_process_fork|kernel_process_free|kernel_process_exit|kernel_arch_kthread_create|list_statedump_end|list_vm_map)
+		CHANNEL=processes
+		;;
+	list_interrupt|statedump_idt_table|statedump_sys_call_table)
+		CHANNEL=interrupts
+		;;
+	list_network_ipv4_interface|list_network_ip_interface)
+		CHANNEL=network
+		;;
+	kernel_module_load|kernel_module_free)
+		CHANNEL=modules
+		;;
+	*)
+		CHANNEL=
+		;;
+	esac
+
+	echo "connect $a default dynamic $CHANNEL" > /proc/ltt
+done
