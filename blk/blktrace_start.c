36d2bf71fcc15160a3a257af70eb3ce43cf66b00
pmf <pmf@04897980-b3bd-0310-b5e0-8ef037075253>
2008-03-27 22:08:37 +0000

    add blktrace_start tool
    used to enable blktrace on a block device
    
    
    git-svn-id: http://ltt.polymtl.ca/svn@2835 04897980-b3bd-0310-b5e0-8ef037075253

---
+++ blk/blktrace_start.c
@@ -0,0 +1,59 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "blktrace_api.h"
+
+int start(int fd)
+{
+	struct blk_user_trace_setup buts;
+
+	memset(&buts, 0, sizeof(buts));
+	buts.buf_size = 512*1024;
+	buts.buf_nr = 4;
+	buts.act_mask = ~0;
+
+	if (ioctl(fd, BLKTRACESETUP, &buts) < 0) {
+		perror("BLKTRACESETUP");
+		return 1;
+	}
+
+	return 0;
+}
+
+int stop(int fd)
+{
+	ioctl(fd, BLKTRACESTOP);
+	if(ioctl(fd, BLKTRACETEARDOWN) < 0)
+		perror("BLKTRACETEARDOWN");
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int fd;
+
+	if(argc < 3) {
+		fprintf(stderr, "usage: --start|--stop %s BLKDEV\n", argv[0]);
+		return 1;
+	}
+
+	fd = open(argv[2], O_RDONLY | O_NONBLOCK);
+	if (fd < 0) {
+		perror(argv[1]);
+		return 1;
+	}
+
+	if(!strcmp("--start", argv[1]))
+		start(fd);
+	else if(!strcmp("--stop", argv[1]))
+		stop(fd);
+
+	close(fd);
+
+	return 0;
+}
