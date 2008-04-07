8dd5f9aaad6a4d587a67500eca0449ec69b7d7bd
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2008-04-07 15:00:21 +0000

    runtest
    git-svn-id: http://ltt.polymtl.ca/svn@2841 04897980-b3bd-0310-b5e0-8ef037075253

--- markers-test/runtest.sh
+++ markers-test/runtest.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+insmod test-mark-speed-empty.ko
+for a in `seq 1 10`; do cat /proc/testmark;done
+rmmod test-mark-speed-empty
+RESEMP=`dmesg |tail -n 10 |sed 's/^\[.*\] cycles : \(.*\)$/\1/'`
+
 insmod test-mark-speed.ko
 for a in `seq 1 10`; do cat /proc/testmark;done
 rmmod test-mark-speed
@@ -16,6 +21,13 @@ rmmod test-mark-speed-edit
 RESNOP=`dmesg |tail -n 10|sed 's/^\[.*\] cycles : \(.*\)$/\1/'`
 
 echo "20000 iterations"
+
+echo "Numbers for empty loop"
+
+SUM="0"
+for a in $RESEMP; do SUM=$[$SUM + $a]; done
+RESEMP=$[$SUM / 10]
+
 
 echo "Numbers for normal marker"
 
