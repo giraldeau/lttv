46f7c1780867c6c52b122caaed47bf846afa1773
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2008-04-07 15:13:30 +0000

    runtest
    git-svn-id: http://ltt.polymtl.ca/svn@2844 04897980-b3bd-0310-b5e0-8ef037075253

--- markers-test/runtest.sh
+++ markers-test/runtest.sh
@@ -3,22 +3,22 @@
 insmod test-mark-speed-empty.ko
 for a in `seq 1 10`; do cat /proc/testmark;done
 rmmod test-mark-speed-empty
-RESEMP=`dmesg |tail -n 10 |sed 's/^\[.*\] cycles : \(.*\)$/\1/'`
+RESEMP=`dmesg |tail -n 10 |sed 's/^\[.*\] //'| sed 's/cycles : \(.*\)$/\1/'`
 
 insmod test-mark-speed.ko
 for a in `seq 1 10`; do cat /proc/testmark;done
 rmmod test-mark-speed
-RESSTD=`dmesg |tail -n 10 |sed 's/^\[.*\] cycles : \(.*\)$/\1/'`
+RESSTD=`dmesg |tail -n 10 |sed 's/^\[.*\] //'| sed 's/cycles : \(.*\)$/\1/'`
 
 insmod test-mark-speed-opt.ko
 for a in `seq 1 10`; do cat /proc/testmark;done
 rmmod test-mark-speed-opt
-RESOPT=`dmesg |tail -n 10|sed 's/^\[.*\] cycles : \(.*\)$/\1/'`
+RESOPT=`dmesg |tail -n 10 |sed 's/^\[.*\] //'| sed 's/cycles : \(.*\)$/\1/'`
 
 insmod test-mark-speed-edit.ko
 for a in `seq 1 10`; do cat /proc/testmark;done
 rmmod test-mark-speed-edit
-RESNOP=`dmesg |tail -n 10|sed 's/^\[.*\] cycles : \(.*\)$/\1/'`
+RESNOP=`dmesg |tail -n 10 |sed 's/^\[.*\] //'| sed 's/cycles : \(.*\)$/\1/'`
 
 echo "20000 iterations"
 
