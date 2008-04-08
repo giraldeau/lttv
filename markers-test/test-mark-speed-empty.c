c2a1736dd3bb48c7fa4647b0a78647f2a4599cc8
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2008-04-08 03:44:09 +0000

    update
    git-svn-id: http://ltt.polymtl.ca/svn@2864 04897980-b3bd-0310-b5e0-8ef037075253

--- markers-test/test-mark-speed-empty.c
+++ markers-test/test-mark-speed-empty.c
@@ -62,7 +62,7 @@ static inline void test(unsigned long arg, unsigned long arg2)
 #endif
 	temp = (temp + 60) << 10;
 	barrier();
-	asm volatile ("");
+	asm ("");
 	barrier();
 	//__my_trace_mark(1, kernel_debug_test, NULL, "%d %d %ld %ld", 2, current->pid, arg, arg2);
 	//__my_trace_mark(0, kernel_debug_test, NULL, "%d %d %ld %ld", 2, current->pid, arg, arg2);
