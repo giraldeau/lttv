f9c31b2e4d7eae8619b22f478266a4b83ec88ee6
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2008-07-14 23:42:47 +0000

    update
    git-svn-id: http://ltt.polymtl.ca/svn@2961 04897980-b3bd-0310-b5e0-8ef037075253

--- ltt-control/lttctl/ltt-armalluser.sh
+++ ltt-control/lttctl/ltt-armalluser.sh
@@ -2,6 +2,8 @@
 #excluding locking markers (high traffic)
 
 echo Connecting all userspace markers of _CURRENTLY RUNNING_ processes only !
+echo All the markers listed here will also automatically be enabled if 
+echo present in a newly created process.
 
 for a in /proc/[0-9]*; do
 	for marker in `cat $a/markers | awk '{print $2}'`; do
