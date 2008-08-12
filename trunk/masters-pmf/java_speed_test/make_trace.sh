9126c96ccf091e596bf536f31b4ba7220b04ef93
pmf <pmf@04897980-b3bd-0310-b5e0-8ef037075253>
2008-08-12 15:41:00 +0000

    make_trace: make larger trace to obtain longer run times
    git-svn-id: http://ltt.polymtl.ca/svn@3003 04897980-b3bd-0310-b5e0-8ef037075253

--- trunk/masters-pmf/java_speed_test/make_trace.sh
+++ trunk/masters-pmf/java_speed_test/make_trace.sh
@@ -1,2 +1,5 @@
 (for i in $(seq 1000); do cat basic_record.dat; done) >tmp.dat
-(for i in $(seq 10); do cat tmp.dat; done) >trace.dat
+(for i in $(seq 500); do cat tmp.dat; done) >tmp2.dat
+rm tmp.dat
+(for i in $(seq 100); do cat tmp2.dat; done) >trace.dat
+rm tmp2.dat
