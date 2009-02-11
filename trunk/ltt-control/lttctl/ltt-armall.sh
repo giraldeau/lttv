7980f12abc86812faddd1ebaf3dccd974297538a
compudj <compudj@04897980-b3bd-0310-b5e0-8ef037075253>
2009-02-11 08:04:26 +0000

    lttctl ltt-armall updated to new debugfs
    git-svn-id: http://ltt.polymtl.ca/svn@3316 04897980-b3bd-0310-b5e0-8ef037075253

--- trunk/ltt-control/lttctl/ltt-armall.sh
+++ trunk/ltt-control/lttctl/ltt-armall.sh
@@ -1,58 +1,47 @@
 #excluding core markers (already connected)
 #excluding locking markers (high traffic)
 
+DEBUGFSROOT=$(grep ^debugfs /proc/mounts | head -1 | awk '{print $2}')
+MARKERSROOT=${DEBUGFSROOT}/ltt/markers
+
 echo Connecting all markers
-MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2 " " $4}'|sort -u|grep -v ^metadata|grep -v ^locking|grep -v ^lockdep`
 
-#separator is newline, ensure compatibility with bash and ash
-N="
-"
-
-IFS=${N}
-for a in $MARKERS; do
-	echo Connecting $a
-
-	echo "connect $a default" > /proc/ltt
-#	#redirect markers carrying state information to dedicated channels
-#	case $a in
-#	list_process_state|list_file_descriptor|user_generic_thread_brand|fs_exec|kernel_process_fork|kernel_process_free|kernel_process_exit|kernel_arch_kthread_create|list_statedump_end|list_vm_map)
-#		CHANNEL=processes
-#		;;
-#	list_interrupt|statedump_idt_table|statedump_sys_call_table|statedump_softirq_vec)
-#		CHANNEL=interrupts
-#		;;
-#	list_network_ipv4_interface|list_network_ip_interface)
-#		CHANNEL=network
-#		;;
-#	list_module|kernel_module_load|kernel_module_free)
-#		CHANNEL=modules
-#		;;
-#	*)
-#		CHANNEL=
-#		;;
-#	esac
-
+for c in ${MARKERSROOT}/*; do
+	case ${c} in
+	${MARKERSROOT}/metadata)
+		;;
+	${MARKERSROOT}/locking)
+		;;
+	${MARKERSROOT}/lockdep)
+		;;
+	*)
+		for m in ${c}/*; do
+			echo Connecting ${m}
+			echo 1 > ${m}/enable
+		done
+		;;
+	esac
 done
 
 
 # Connect the interesting high-speed markers to the marker tap.
 # Markers starting with "tap_" are considered high-speed.
-echo Connecting high-rate markers to tap
-MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u |grep ^tap_`
-
-#Uncomment the following to also record lockdep events.
-#MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -e ^tap_ -e ^lockdep`
-
-IFS=${N}
-for a in $MARKERS; do
-	echo Connecting $a
-
-	#redirect markers carrying state information to dedicated channels
-	case $a in
-	*)
-		CHANNEL=
-		;;
-	esac
-
-	echo "connect $a ltt_tap_marker" > /proc/ltt
-done
+#echo Connecting high-rate markers to tap
+#MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u |grep ^tap_`
+#
+##Uncomment the following to also record lockdep events.
+##MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -e ^tap_ -e ^lockdep`
+#
+#IFS=${N}
+#for a in $MARKERS; do
+#	echo Connecting $a
+#
+#	#redirect markers carrying state information to dedicated channels
+#	case $a in
+#	*)
+#		CHANNEL=
+#		;;
+#	esac
+#
+#	echo "connect $a ltt_tap_marker" > /proc/ltt
+#done
--- trunk/ltt-control/lttctl/ltt-disarmall.sh
+++ trunk/ltt-control/lttctl/ltt-disarmall.sh
@@ -1,26 +1,38 @@
-#excluding locking
-#excluding core markers, not connected to default.
+#excluding core markers (already connected)
+#excluding locking markers (high traffic)
+
+DEBUGFSROOT=$(grep ^debugfs /proc/mounts | head -1 | awk '{print $2}')
+MARKERSROOT=${DEBUGFSROOT}/ltt/markers
+
 echo Disconnecting all markers
-MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2 " " $4}'|sort -u|grep -v ^metadata|grep -v ^locking|grep -v ^lockdep|grep -v ^tap`
 
-
-#separator is newline, ensure compatibility with bash and ash
-N="
-"
-
-IFS=${N}
-for a in $MARKERS; do echo Disconnecting $a; echo "disconnect $a" > /proc/ltt; done
-
-# Markers starting with "tap_" are considered high-speed.
-echo Disconnecting high-rate markers to tap
-MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2 " " $4}'|sort -u |grep ^tap`
-
-#Uncomment the following to also stop recording lockdep events.
-#MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -e ^tap_ -e ^lockdep`
-
-IFS=${N}
-for a in $MARKERS; do
-	echo Disconnecting $a
-
-	echo "disconnect $a ltt_tap_marker" > /proc/ltt
+for c in ${MARKERSROOT}/*; do
+	case ${c} in
+	${MARKERSROOT}/metadata)
+		;;
+	${MARKERSROOT}/locking)
+		;;
+	${MARKERSROOT}/lockdep)
+		;;
+	*)
+		for m in ${c}/*; do
+			echo Disconnecting ${m}
+			echo 0 > ${m}/enable
+		done
+		;;
+	esac
 done
+
+## Markers starting with "tap_" are considered high-speed.
+#echo Disconnecting high-rate markers to tap
+#MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2 " " $4}'|sort -u |grep ^tap`
+#
+##Uncomment the following to also stop recording lockdep events.
+##MARKERS=`cat /proc/ltt|grep -v %k|awk '{print $2}'|sort -u|grep -e ^tap_ -e ^lockdep`
+#
+#IFS=${N}
+#for a in $MARKERS; do
+#	echo Disconnecting $a
+#
+#	echo "disconnect $a ltt_tap_marker" > /proc/ltt
+#done
