7f16dc899ade9895d3820f5c9e3a9593ef5c08f2
William Bourque <william.bourque@polymtl.ca>
2010-02-05 12:05:11 -0500

    Added missing licence header.

--- ltt/jni_interface.c
+++ ltt/jni_interface.c
@@ -1,3 +1,20 @@
+/* This file is part of the Linux Trace Toolkit viewer
+ * Copyright (C) 2010 William Bourque
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License Version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
 
 /* Important to get consistent size_t type */
 #define _FILE_OFFSET_BITS 64
@@ -70,7 +87,6 @@ struct saveTimeAndTracefile
         LttTime time;
         LttTracefile *tracefile;
 };
-
 
 /* 
 ### COMMON Methods ###
